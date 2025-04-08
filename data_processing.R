013d3b314760e58c8015bf176bd081838592f3af
Edward Mason <mynameisprivate3@gmail.com>
2025-04-08 15:22:21 +0100

    Init

---
+++ data_processing.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(sf)
+library(maptiles)
+library(tidyterra)
+
+rm(list = ls())
+setwd("~/Documents/University/Geography Degree/Year 1/Semester 2/40315 LC Fieldwork Project Design (FPD)/Assessment/Data Analysis")
+file <- read.csv("Data.csv", header = TRUE, sep = ",")
+data <- tibble(file)
+
+# Convert DateTime to a date-time object -> year-month-day, hh:mm:ss timezone
+data$DateTime <- as.POSIXct(data$DateTime, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Amsterdam")
+
+# Split Coordinates column into lat long
+data <- data %>%
+  separate(Coordinates, into = c("Latitude", "Longitude"), sep = ",") %>%
+  mutate(Latitude = as.numeric(Latitude),
+         Longitude = as.numeric(Longitude))
+
+points <- st_as_sf(data, coords = c("Longitude","Latitude"), crs=4326)
+basemap <- get_tiles(points, provider="OpenStreetMap")
+
+ggplot() +
+  geom_spatraster_rgb(data = basemap) +
+  geom_sf(data = points, colour = "red", size = 0.5) +
+  coord_sf(crs = 3857) + 
+  labs(title = "Sampling Locations", caption = "\U00a9 OpenStreetMap contributors") + 
+  theme_void()
